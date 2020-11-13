f4ba92b01df854e763dfbede33b00ec5ec145770
Christian Sumido <gcbsumid@gmail.com>
2020-11-12 18:30:34 -0800

    OVCToken made into an abstract base contract so new tokens can be created

--- contracts/Tokens/OVCTokenBase.sol
+++ contracts/Tokens/OVCTokenBase.sol
@@ -4,14 +4,14 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract OVCToken is ERC20, AccessControl
+abstract contract OVCTokenBase is ERC20, AccessControl
 {
     // Create a new role identifier for the minter role. Limiting what each component of a system 
     // can do is known as "principle of least privilege" and is good security practice.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor(uint256 initialSupply) public ERC20("Omniverse Credits", "OVC")
+    constructor(string memory name, string memory symbol, uint256 initialSupply) public ERC20(name, symbol)
     {
         // Contract Deployer is now the owner and can set roles
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -19,7 +19,6 @@ contract OVCToken is ERC20, AccessControl
         // mint initial supply of tokens
         _mint(msg.sender, initialSupply);
     }
-    
 
     function mint(address to, uint256 amount) public 
     {
