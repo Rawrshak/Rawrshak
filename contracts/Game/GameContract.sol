44783f63a4be838c931e20be1f19edc3d3d35d9e
Christian Sumido <gcbsumid@gmail.com>
2020-11-12 21:34:52 -0800

    Created Item Structure and use EnumerableSet to hold it

--- contracts/Game/GameContract.sol
+++ contracts/Game/GameContract.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 
 struct Item
 {
@@ -13,11 +14,19 @@ struct Item
     mapping(address => uint) _balances;
 }
 
+
 contract GameContract is ERC1155, AccessControl
 {
+    using EnumerableSet for EnumerableSet.UintSet;
+    struct ItemSet {
+        EnumerableSet.UintSet _inner;
+        mapping(uint256 => Item) ItemList;
+    }
+
     /******** Stored Variables ********/
     // This is the address where transactions are sent to.
-    address payable private GamePayableAddress;
+    address payable private _gamePayableAddress;
+    ItemSet private _itemSet;
 
     /******** Events ********/
     event GamePayableAddressChanged(address);
@@ -28,12 +37,13 @@ contract GameContract is ERC1155, AccessControl
     // developer app interfaces with.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant ITEM_MANAGER_ROLE = keccak256("ITEM_MANAGER_ROLE");
 
     // url: "https://game.example/api/item/{id}.json"
     constructor(string memory url) public ERC1155(url)
     {
         // Contract Deployer is now the owner and can set roles
-        GamePayableAddress = msg.sender;
+        _gamePayableAddress = msg.sender;
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(GAME_OWNER_ROLE, msg.sender);
     }
@@ -41,15 +51,30 @@ contract GameContract is ERC1155, AccessControl
     // GamePayableAddress getter
     function getGamePayableAddress() public view returns(address payable)
     {
-        return GamePayableAddress;
+        return _gamePayableAddress;
     }
 
     // GamePayableAddress setter
     function setGamePayableAddress(address payable newAddress) public
     {
-        require(hasRole(GAME_OWNER_ROLE, msg.sender), "User does not have the necessary permissions.");
-        GamePayableAddress = newAddress;
+        require(hasRole(GAME_OWNER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        _gamePayableAddress = newAddress;
         emit GamePayableAddressChanged(newAddress);
+    }
+
+    // chrsum-todo: figure out whether I want to do create an ItemList Structure or not.
+    // Create New Item
+    function createItem(uint256 uuid) public returns (bool) {
+        require(hasRole(ITEM_MANAGER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        require(_itemSet._inner.add(uuid), "This item already exists.");
+
+        Item storage item = _itemSet.ItemList[uuid];
+        item._uuid = uuid;
+        item._url = this.uri(uuid);
+        item._totalSupply = 0;
+        item._creatorAddress = _gamePayableAddress;
+
+        return true;
     }
 
     // Todo: Add/remove items
