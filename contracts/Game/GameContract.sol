d42d695f1f2e70bc221d4414d00971b534b68e0e
Christian Sumido <gcbsumid@gmail.com>
2020-11-13 22:03:52 -0800

    added mint and burn functionality and tests

--- contracts/Game/GameContract.sol
+++ contracts/Game/GameContract.sol
@@ -5,22 +5,21 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 
+// Todo: Figure out mechanism whether to cap item supply or not.
 struct Item
 {
     uint256 _uuid;
     string _url;
     uint256 _totalSupply;
     address payable _creatorAddress;
-    mapping(address => uint) _balances;
 }
-
 
 contract GameContract is ERC1155, AccessControl
 {
     using EnumerableSet for EnumerableSet.UintSet;
     struct ItemSet {
         EnumerableSet.UintSet _inner;
-        mapping(uint256 => Item) ItemList;
+        mapping(uint256 => Item) _itemList;
     }
 
     /******** Stored Variables ********/
@@ -64,27 +63,28 @@ contract GameContract is ERC1155, AccessControl
 
     // chrsum-todo: figure out whether I want to do create an ItemList Structure or not.
     // Create New Item
-    function createItem(uint256 uuid) public returns (bool)
+    function createItem(uint256 uuid, address payable creatorAddress) public returns (bool)
     {
         require(hasRole(ITEM_MANAGER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
         require(_itemSet._inner.add(uuid), "This item already exists.");
 
-        Item storage item = _itemSet.ItemList[uuid];
+        Item storage item = _itemSet._itemList[uuid];
         item._uuid = uuid;
         item._url = this.uri(uuid);
         item._totalSupply = 0;
-        item._creatorAddress = _gamePayableAddress;
+        item._creatorAddress = (creatorAddress == address(0)) ? _gamePayableAddress : creatorAddress;
 
         return true;
     }
 
+    // CHRSUM - should I be able to delete items from under the players?
     // Delete the item
     function removeItem(uint256 uuid) public returns (bool)
     {
         require(hasRole(ITEM_MANAGER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
         require(_itemSet._inner.contains(uuid), "This item does not exist.");
         
-        delete _itemSet.ItemList[uuid];
+        delete _itemSet._itemList[uuid];
         
         return _itemSet._inner.remove(uuid);
     }
@@ -116,8 +116,71 @@ contract GameContract is ERC1155, AccessControl
         return uuidList;
     }
 
-    // Todo: Add item creation/deletion tests
-    // Todo: mint and burn items
-    // Todo: Add item minting/burning tests
-    // Todo: Query Items
+    function getTotalSupply(uint256 id) public view returns(uint256)
+    {
+        require(_itemSet._inner.contains(id), "This item does not exist.");
+        return _itemSet._itemList[id]._totalSupply;
+    }
+
+    function getCreatorAddress(uint256 id) public view returns(address)
+    {
+        require(_itemSet._inner.contains(id), "This item does not exist.");
+        return _itemSet._itemList[id]._creatorAddress;
+    }
+
+    function mint(address receivingAddress, uint256 itemId, uint256 amount) public 
+    {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        require(_itemSet._inner.contains(itemId), "Item does not exist.");
+        require(receivingAddress != address(0), "Cannot send to null adderss.");
+
+        _itemSet._itemList[itemId]._totalSupply += amount;
+        _mint(receivingAddress, itemId, amount, "");
+    }
+
+    function mintBatch(address receivingAddress, uint256[] memory itemIds, uint256[] memory amounts) public 
+    {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        require(itemIds.length == amounts.length, "item arrays don't match.");
+        require(receivingAddress != address(0), "Cannot send to null adderss.");
+
+        for (uint i = 0; i < itemIds.length; i++)
+        {
+            require(_itemSet._inner.contains(itemIds[i]), "Item does not exist.");
+            _itemSet._itemList[itemIds[i]]._totalSupply += amounts[i];
+        }
+        _mintBatch(receivingAddress, itemIds, amounts, "");
+    }
+
+    // Todo: Burning needs approval from the account
+    function burn(address account, uint256 itemId, uint256 amount) public 
+    {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        require(_itemSet._inner.contains(itemId), "Item does not exist.");
+        require(account != address(0), "Cannot send to null adderss.");
+
+        // _burn requirements are that account is non-zero and account has enough of these items
+        _burn(account, itemId, amount);
+
+        // if _burn succeeds, then we know that _totalSupply can be deducted.
+        _itemSet._itemList[itemId]._totalSupply -= amount;
+    }
+
+    // Todo: Burning needs approval from the account
+    function burnBatch(address account, uint256[] memory itemIds, uint256[] memory amounts) public 
+    {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller does not have the necessary permissions.");
+        require(itemIds.length == amounts.length, "item arrays don't match.");
+        require(account != address(0), "Cannot send to null adderss.");
+
+        // _burnBatch requirements are that account is non-zero and account has enough of these items
+        _burnBatch(account, itemIds, amounts);
+
+        // if _burnBatch succeeds, then we know that _totalSupply can be deducted.
+        for (uint i = 0; i < itemIds.length; i++)
+        {
+            require(_itemSet._inner.contains(itemIds[i]), "Item does not exist.");
+            _itemSet._itemList[itemIds[i]]._totalSupply -= amounts[i];
+        }
+    }
 }
--- test/GameTests/GameContractTests.js
+++ test/GameTests/GameContractTests.js
@@ -25,7 +25,7 @@ contract('Game Contract', (accounts) => {
         const deployerAddress = accounts[0];
         const newOwnerAddress = accounts[1];
 
-        await gameContract.setGamePayableAddress(newOwnerAddress, {from:deployerAddress});
+        await gameContract.setGamePayableAddress(newOwnerAddress, {from:deployerAddress, gasPrice: 1});
         const gameOwnerAddress = await gameContract.getGamePayableAddress();
 
         assert.equal(gameOwnerAddress, newOwnerAddress, "Game Owner Address was not updated.");
@@ -48,10 +48,10 @@ contract('Game Contract', (accounts) => {
             true, "first account didn't have admin role");
 
         // account 0 grants account 1 a role
-        await gameContract.grantRole(minter_role, minterAddress, {from:deployerAddress});
+        await gameContract.grantRole(minter_role, minterAddress, {from:deployerAddress, gasPrice: 1});
         
         // account 0 grants account 2 a role
-        await gameContract.grantRole(burner_role, burnerAddress, {from:deployerAddress});
+        await gameContract.grantRole(burner_role, burnerAddress, {from:deployerAddress, gasPrice: 1});
 
         // check to see if account 1 is the minter role
         assert.equal(
@@ -68,7 +68,7 @@ contract('Game Contract', (accounts) => {
             true, "account 2 didn't have the burner role");
     });
 
-    it('Create 2 Item', async () => {
+    it('Create 3 Item', async () => {
         const gameContract = await GameContract.deployed();
         const item_manager_role = await gameContract.ITEM_MANAGER_ROLE();
 
@@ -76,7 +76,7 @@ contract('Game Contract', (accounts) => {
         const itemManagerAddress = accounts[4];
 
         // deployer address grants item manager address a the item manager role
-        await gameContract.grantRole(item_manager_role, itemManagerAddress, {from:deployerAddress});
+        await gameContract.grantRole(item_manager_role, itemManagerAddress, {from:deployerAddress, gasPrice: 1});
 
         // check to see if item manager address has the item manger role
         assert.equal(
@@ -86,13 +86,16 @@ contract('Game Contract', (accounts) => {
             true, "Item Manager Address didn't have the Item Manager Role");
 
         // Create 2 New Items
-        await gameContract.createItem(1, {from:itemManagerAddress});
-        await gameContract.createItem(2, {from:itemManagerAddress});
+        // 0 is the solidity equivalent of address(0)
+        await gameContract.createItem(1, '0x0000000000000000000000000000000000000000', {from:itemManagerAddress, gasPrice: 1});
+        await gameContract.createItem(2, '0x0000000000000000000000000000000000000000', {from:itemManagerAddress, gasPrice: 1});
+        await gameContract.createItem(3, '0x0000000000000000000000000000000000000000', {from:itemManagerAddress, gasPrice: 1});
 
         // Check if the new items were added.
-        assert.equal((await gameContract.length()).toNumber(), 2, "The 2 new items were not created.");
+        assert.equal((await gameContract.length()).toNumber(), 3, "The 2 new items were not created.");
         assert.equal(await gameContract.exists(1), true, "Item 1 wasn't created.");
         assert.equal(await gameContract.exists(2), true, "Item 2 wasn't created.");
+        assert.equal(await gameContract.exists(3), true, "Item 3 wasn't created.");
     });
 
     // Should I be able to delete an item? probably not.
@@ -102,7 +105,7 @@ contract('Game Contract', (accounts) => {
 
         const itemManagerAddress = accounts[4];
 
-        // check to see if account 1 is the minter role
+        // check to see if item manager address has the item manger role
         assert.equal(
             await gameContract.hasRole(
                 item_manager_role,
@@ -110,11 +113,104 @@ contract('Game Contract', (accounts) => {
             true, "Item Manager Address didn't have the Item Manager Role");
 
         // Delete item with UUID 2
-        await gameContract.removeItem(2, {from:itemManagerAddress});
+        await gameContract.removeItem(3, {from:itemManagerAddress, gasPrice: 1});
 
         // Check if the new items were added.
-        assert.equal((await gameContract.length()).toNumber(), 1, "There is only 1 item left.");
+        assert.equal((await gameContract.length()).toNumber(), 2, "There is only 1 item left.");
         assert.equal(await gameContract.exists(1), true, "Item 1 was deleted.");
-        assert.equal(await gameContract.exists(2), false, "Item 2 was not deleted.");
+        assert.equal(await gameContract.exists(2), true, "Item 2 was deleted.");
+        assert.equal(await gameContract.exists(3), false, "Item 2 was not deleted.");
+    });
+
+    it('Mint 10 Items of Item 1', async () => {
+        const gameContract = await GameContract.deployed();
+        const minter_role = await gameContract.MINTER_ROLE();
+        const minterAddress = accounts[2];
+        const playerAddress = accounts[5];
+        const player2Address = accounts[6];
+
+        // check to see if minter address is the minter role
+        assert.equal(
+            await gameContract.hasRole(
+                minter_role,
+                minterAddress),
+            true, "Minter Address didn't have the Minter Role");
+
+        // mint 10 items
+        await gameContract.mint(playerAddress, 1, 10, {from:minterAddress, gasPrice: 1});
+
+        // check if the item was minted
+        assert.equal(
+            (await gameContract.balanceOf(
+                playerAddress,
+                1)).toNumber(),
+            10, "10 supply of Item 1 was not minted properly.");
+
+        assert.equal(
+            (await gameContract.getTotalSupply(1)).toNumber(),
+            10, "10 supply of Item 1 was not minted properly.");
+        
+        // mint 10 items of item id 1 and 2
+        await gameContract.mintBatch(player2Address, [1,2], [10,10], {from:minterAddress, gasPrice: 1});
+        assert.equal(
+            (await gameContract.balanceOf(
+                player2Address,
+                1)).toNumber(),
+            10, "10 supply of Item 1 was not minted properly.");
+        assert.equal(
+            (await gameContract.balanceOf(
+                player2Address,
+                2)).toNumber(),
+            10, "10 supply of Item 2 was not minted properly.");
+
+        assert.equal(
+            (await gameContract.getTotalSupply(1)).toNumber(),
+            20, "The total supply of Item 1 is not correct.");
+    });
+
+    it('Burn 10 Items of Item 1', async () => {
+        const gameContract = await GameContract.deployed();
+        const burner_role = await gameContract.BURNER_ROLE();
+        const burnerAddress = accounts[3];
+        const playerAddress = accounts[5];
+        const player2Address = accounts[6];
+
+        // check to see if burner address is the burner role
+        assert.equal(
+            await gameContract.hasRole(
+                burner_role,
+                burnerAddress),
+            true, "Burner Address didn't have the Burner Role");
+
+        // burn 10 items
+        await gameContract.burn(playerAddress, 1, 10, {from:burnerAddress, gasPrice: 1});
+
+        // check if the item was burned
+        assert.equal(
+            (await gameContract.balanceOf(
+                playerAddress,
+                1)).toNumber(),
+            0, "10 supply of Item 1 was not burned properly.");
+
+        assert.equal(
+            (await gameContract.getTotalSupply(1)).toNumber(),
+            10, "10 supply of Item 1 was not burned properly.");
+        
+        // burn 10 items of item id 1 and 2
+        await gameContract.burnBatch(player2Address, [1,2], [10,10], {from:burnerAddress, gasPrice: 1});
+        assert.equal(
+            (await gameContract.balanceOf(
+                player2Address,
+                1)).toNumber(),
+            0, "20 supply of Item 1 was not burned properly.");
+        assert.equal(
+            (await gameContract.balanceOf(
+                player2Address,
+                2)).toNumber(),
+            0, "10 supply of Item 2 was not burned properly.");
+
+        assert.equal(
+            (await gameContract.getTotalSupply(1)).toNumber(),
+            0, "The total supply of Item 1 is not correct.");
     });
 });
