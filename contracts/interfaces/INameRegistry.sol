a3d2c83127a7ee87a66e62606c5beb9331c57af3
Christian Sumido <gcbsumid@gmail.com>
2021-04-03 17:09:18 -0700

    Update the solidity version requirement

--- contracts/interfaces/INameRegistry.sol
+++ contracts/interfaces/INameRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 interface INameRegistry {
     function registerName(string calldata _name, address _addr, uint16 _ver) external;
