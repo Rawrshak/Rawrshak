08889b47ccf4d22d7055f7832ed7d6ccd117f8d6
Christian Sumido <gcbsumid@gmail.com>
2021-04-28 10:28:15 -0700

    Deleting the Deprecated code

--- deprecated/contracts/interfaces/IGameManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.9.0;
-
-import "./IGameInterface.sol";
-
-interface IGameManager is IGameInterface {
-    function generateGameContract(address _gameFactoryAddress, string calldata _url) external;
-}
