84a03652d27575e1d573dad59fb38f665cad22a5
Christian Sumido <gcbsumid@gmail.com>
2021-09-05 19:08:14 -0700

    update registry tests

--- test/registry/ContractRegistryTests.js
+++ test/registry/ContractRegistryTests.js
@@ -44,14 +44,14 @@ contract('Contract Registry Tests', (accounts) => {
         await contentStorage.__ContentStorage_init([[deployerAddress, web3.utils.toWei('0.01', 'ether')]], "arweave.net/tx-contract-uri");
         content = await Content.new();
         await content.__Content_init("Test Content Contract", "TEST", contentStorage.address, accessControlManager.address);
-        contentStorage.setParent(content.address);
-        accessControlManager.setParent(content.address);
+        await contentStorage.setParent(content.address);
 
         contentManager = await ContentManager.new();
         await contentManager.__ContentManager_init(content.address, contentStorage.address, accessControlManager.address, tagsManager.address, {from: deployerAddress});
         await contentStorage.grantRole(await contentStorage.OWNER_ROLE(), contentManager.address, {from: deployerAddress});
-        await accessControlManager.grantRole(await accessControlManager.OWNER_ROLE(), contentManager.address, {from: deployerAddress});
-        
+        await accessControlManager.grantRole(await accessControlManager.DEFAULT_ADMIN_ROLE(), contentManager.address, {from: deployerAddress});
+        await accessControlManager.setParent(content.address);
+
         craft = await Craft.new();
         await craft.__Craft_init(1000);
         
