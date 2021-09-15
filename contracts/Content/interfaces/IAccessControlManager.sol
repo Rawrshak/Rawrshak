e02967e85b9d331632ffdf6164100b3b375d9f4c
Christian Sumido <gcbsumid@gmail.com>
2021-09-15 12:34:53 -0700

    Update percentages from uint256 to uint24

--- contracts/Content/interfaces/IAccessControlManager.sol
+++ contracts/Content/interfaces/IAccessControlManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "../../libraries/LibRoyalties.sol";
 import "../../libraries/LibAsset.sol";
 import "./IContentSubsystemBase.sol";
 
