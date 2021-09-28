22a12d314a6bedb92aa0348966ecbfb3035bf1be
Christian Sumido <gcbsumid@gmail.com>
2021-09-28 13:29:12 -0700

    Update readme

--- test-staking-contracts.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-echo Running Exchange Contracts Tests...
-
-call truffle compile
-
-call truffle test --compile-none --debug ./test/staking/ExchangeFeesEscrowTests.js
-call truffle test --compile-none --debug ./test/staking/LiquidityMiningTests.js 
-call truffle test --compile-none --debug ./test/staking/StakingTests.js
