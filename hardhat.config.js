ffae941da9c07c477d4e106ff98c6855efda37e6
Christian Sumido <gcbsumid@gmail.com>
2021-10-19 09:21:47 -0700

    Run tests against local optimism L2 node

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,46 @@ require("@nomiclabs/hardhat-web3");
 require('@typechain/hardhat');
 require('@nomiclabs/hardhat-ethers');
 
+// Load environment variables from .env
+require('dotenv').config();
+
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.3",
+    networks: {
+        hardhat: {
+            accounts: {
+                mnemonic: 'test test test test test test test test test test test junk'
+            }
+        },
+        optimistic_local: {
+            url: 'http://127.0.0.1:8545',
+            accounts: {
+                mnemonic: 'test test test test test test test test test test test junk'
+            },
+            gasPrice: 0
+        },
+        optimistic_kovan: {
+            chainId: 69,
+            url: 'https://kovan.optimism.io',
+            accounts: [process.env.KOVAN_PRIVATE_KEY],
+            gasPrice: 15000000
+        },
+        kovan: {
+            chainId: 42,
+            url: 'https://kovan.infura.io/v3/e81f88c751474c8695f35f6e5193b0e0',
+            accounts: [process.env.KOVAN_PRIVATE_KEY]
+        }
+    },
+    solidity: "0.8.3"
 };
