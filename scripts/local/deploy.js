53e3533ad32ceb574d49a95e5df34f42a286aff5
Christian Sumido <gcbsumid@gmail.com>
2021-10-25 12:27:27 -0700

    Launch test contracts on Optimistic Kovan testnet

---
+++ scripts/local/deploy.js
@@ -0,0 +1,146 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    // // Todo: Replace Private key with the Testnet Deployer wallet
+    // const provider = new ethers.providers.JsonRpcProvider(hre.network.config.url)
+    // const deployer = new ethers.Wallet(process.env.KOVAN_PRIVATE_KEY, provider)
+    
+    console.log(`Deploying contracts with the account: ${deployer.address}`);
+  
+    const balance = await deployer.getBalance();
+    console.log(`Account Balance: ${web3.utils.fromWei(balance.toString(), 'ether')}`);
+    console.log("Deploying RAWR token contract");
+
+    MockToken = await ethers.getContractFactory("MockToken");
+    rawr = await upgrades.deployProxy(MockToken, ["Rawrshak Token", "RAWR"]);
+    await rawr.mint(deployer.address, web3.utils.toWei("100000000", 'ether'));
+
+    // Todo: Replace this with the correct RAWR token address
+    // const rawr = MockToken.attach("0x...");
+    console.log("RAWR Token deployed to:", rawr.address);
+    console.log("\n");
+    
+    console.log("Deploying Address Resolver Contract");
+    const AddressResolver = await ethers.getContractFactory("AddressResolver");
+    const resolver = await upgrades.deployProxy(AddressResolver, []);
+    console.log("AddressResolver deployed to:", resolver.address);
+    console.log("\n");
+    
+
+    console.log("Deploying Exchange Contracts");
+    const NftEscrow = await ethers.getContractFactory("NftEscrow");
+    const Erc20Escrow = await ethers.getContractFactory("Erc20Escrow");
+    const ExecutionManager = await ethers.getContractFactory("ExecutionManager");
+    const Orderbook = await ethers.getContractFactory("Orderbook");
+    const RoyaltyManager = await ethers.getContractFactory("RoyaltyManager");
+    const Exchange = await ethers.getContractFactory("Exchange");
+    const ExchangeFeesEscrow = await ethers.getContractFactory("ExchangeFeesEscrow");
+    
+    // Deploy Internal Exchange contracts
+    const nftEscrow = await upgrades.deployProxy(NftEscrow, []);
+    const tokenEscrow = await upgrades.deployProxy(Erc20Escrow, []);
+    const feesEscrow = await upgrades.deployProxy(ExchangeFeesEscrow, [resolver.address]);
+    const orderbook = await upgrades.deployProxy(Orderbook, [resolver.address]);
+    const executionManager = await upgrades.deployProxy(ExecutionManager, [resolver.address]);
+    const royaltyManager = await upgrades.deployProxy(RoyaltyManager, [resolver.address]);
+
+    var addresses = [
+        tokenEscrow.address,
+        nftEscrow.address,
+        feesEscrow.address,
+        orderbook.address,
+        executionManager.address,
+        royaltyManager.address,
+    ];
+
+    var escrowIds = [
+        "0x29a264aa",
+        "0x87d4498b",
+        "0x7f170836",
+        "0xd9ff7618",
+        "0x018869a9",
+        "0x2c7e992e"
+    ];
+    await resolver.registerAddress(escrowIds, addresses);
+
+    // Register the managers
+    await nftEscrow.registerManager(executionManager.address);
+    await tokenEscrow.registerManager(executionManager.address);
+    await tokenEscrow.registerManager(royaltyManager.address);
+    await feesEscrow.registerManager(royaltyManager.address);
+
+    // Deploy Exchange contract
+    const exchange = await upgrades.deployProxy(Exchange, [royaltyManager.address, orderbook.address, executionManager.address]);
+    
+    // set ownership of managers to exchange contract
+    await royaltyManager.transferOwnership(exchange.address);
+    await orderbook.transferOwnership(exchange.address);
+    await executionManager.transferOwnership(exchange.address);
+    
+    // Add RAWR token as a supported payment option
+    await exchange.addSupportedToken(rawr.address);
+
+    console.log("Exchange deployed to:", exchange.address);
+    console.log("\n");
+
+    console.log("Deploying Staking Contracts");
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = await upgrades.deployProxy(Staking, [rawr.address, resolver.address]);
+    await feesEscrow.registerManager(staking.address);
+    console.log("Staking deployed to:", staking.address);
+    console.log("\n");
+
+    // Content Contracts
+    console.log("Deploying Content Contracts");
+    
+    const AccessControlManager = await ethers.getContractFactory("AccessControlManager");
+    const ContentStorage = await ethers.getContractFactory("ContentStorage");
+    const Content = await ethers.getContractFactory("Content");
+    const ContentManager = await ethers.getContractFactory("ContentManager");
+    const ContentFactory = await ethers.getContractFactory("ContentFactory");
+
+    // Deploy Implementation contracts
+    const accessControlManagerImpl = await AccessControlManager.deploy();
+    const contentImpl = await Content.deploy();
+    const contentStorageImpl = await ContentStorage.deploy();
+    const contentManagerImpl = await ContentManager.deploy();
+
+    // Initialize Content Clone Factory
+    const contentFactory = await upgrades.deployProxy(ContentFactory, [contentImpl.address, contentManagerImpl.address, contentStorageImpl.address, accessControlManagerImpl.address]);
+
+    console.log("ContentFactory deployed to:", contentFactory.address);
+    console.log("\n");
+
+    // setting up resolver addresses
+    // register the exchange contracts on the address resolver
+    addresses = [
+        exchange.address,
+        staking.address,
+        contentFactory.address,
+        rawr.address
+    ];
+
+    escrowIds = [
+        "0xeef64103",
+        "0x1b48faca",
+        "0xdb337f7d",
+        "0x3d13c043"
+    ];
+    await resolver.registerAddress(escrowIds, addresses);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+// Deployment Contracts on Optimism Local Node
+// RAWR Token deployed to: 0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f
+// AddressResolver deployed to: 0x09635F643e140090A9A8Dcd712eD6285858ceBef
+// Exchange deployed to: 0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00
+// Staking deployed to: 0xb7278A61aa25c888815aFC32Ad3cC52fF24fE575
+// ContentFactory deployed to: 0xFD471836031dc5108809D173A067e8486B9047A3
---
+++ scripts/optimistic-kovan/subgraph_ipfs_data.js
@@ -0,0 +1,153 @@
+const { ethers, upgrades } = require("hardhat");
+
+var Content;
+var ContentManager;
+
+async function deployContract(factory, developer, rate, uri) {
+
+    var tx = await factory.connect(developer).createContracts(developer.address, rate, uri);
+    var receipt = await tx.wait();
+    var deployedContracts = receipt.events?.filter((x) => {return x.event == "ContractsDeployed"});
+
+    // To figure out which log contains the ContractDeployed event
+    var content = await Content.attach(deployedContracts[0].args.content);
+    var contentManager = await ContentManager.attach(deployedContracts[0].args.contentManager);
+
+    var approvalPair = [[developer.address, true]];
+    await contentManager.connect(developer).registerOperators(approvalPair);
+
+    return {content, contentManager};
+}
+
+async function addRawrshakAssets(content, contentManager, developer) {
+    var asset = [
+        [0, "QmTgLKGxFapNAaKSoaXZkx3vq9xiarfag3cjcByUywzf7h", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Apprentice Title
+        [1, "QmX1Epc144wovBa3i8LVy6JoVxUpVjuA7nQvDmigo8NAx7", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Creator Title
+        [2, "QmaCGZKewUkJCw7nHtvMCTxxeeg1fNhF1BGKVowAAh5dJn", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Gamer Pesant Title
+        [3, "QmeZ9t91M5AizHBwPHt7ZYEzvhS9ic98eQ8BUQ7bZrERBT", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Disciple Title
+        [4, "QmUcUdn8Gf9hiwqvmN9SzP4JZsrC72CpPLptXwjyW8Nuv3", "", ethers.constants.MaxUint256, developer.address, 20000],          // Lord Title
+        [5, "QmWGDc6HM4oXXmhkDYKLcL95anW5Wsxmw6daUrc2KL82fa", "", ethers.constants.MaxUint256, developer.address, 20000]           // Original Sinner Title
+    ];
+
+    // add assets
+    await contentManager.connect(developer).addAssetBatch(asset);
+    
+    // mint some assets
+    var mintData = [developer.address, [0,1,2,3,4,5], [100,100,100,100,10,2], 0, ethers.constants.AddressZero, []];
+    await content.connect(developer).mintBatch(mintData);
+}
+
+async function addScreamFortress2Assets(content, contentManager, developer) {
+    var asset = [
+        [0, "QmPdzEWnW1PAxm4LFe5pRw3g5kNee8oKKtbs92sVekMce4", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Demoman
+        [1, "QmPC6iFFc1ewqVCcfPyyG4YssL6zwX9DW2VQDD5K6P3PLD", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Engineer
+        [2, "QmXVeyvvDhknXuH2CevPUGxjDFXDPRTxS7k6Ehm7jRVSHH", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Heavy
+        [3, "QmVqWJPhcchCSp22FQSxxzRZMFVpae2fMSa3sPbmHywXCC", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Medic
+        [4, "QmbTvdvL2CUPpBVf7LvFb25t2JdqqGjumed5bVaSpyCZYx", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Pyro
+        [5, "QmWroKamTeD2hr66ERDGXG7axLzagK2p48BBuBwjN7TFrF", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Scout
+        [6, "QmaUz6wBvxTFJBdcJRy59nMPNm78CKMjwuqYgfvGBUwZ52", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Sniper
+        [7, "QmNbQxN82JweyESPP9tCVMroVA1itf974pLknMjkt3Eo2k", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0],   // Soldier
+        [8, "QmSPTeM11qMLKWMtqrvPDV1FxEDEkjQ5HZjJmQfkGq4DZt", "", ethers.constants.MaxUint256, ethers.constants.AddressZero, 0]    // Spy
+    ];
+
+    // add assets
+    await contentManager.connect(developer).addAssetBatch(asset);
+    
+    // mint some assets
+    var mintData = [developer.address, [0,1,2,3,4,5,6,7,8], [100,100,100,100,100,100,100,100,100], 0, ethers.constants.AddressZero, []];
+    await content.connect(developer).mintBatch(mintData);
+}
+
+async function addFightBuddyAssets(content, contentManager, developer) {
+    var asset = [
+        [0, "QmPFJoS7UKXmmXeA4HMes1K5GRNM2QQ94hf6dSUqH3i2wF", "", 1000, developer.address, 10000],         // Nikolai
+        [1, "Qma3cwsq1kqcAP6zudnr1WyWWkXhVnYa1wgRAun8op9nGa", "", 1000, developer.address, 20000],         // Didier
+        [2, "Qma7QXrm9wUFmdxBWT32fWccfQ6kvNeZS598eMdVuBvthU", "", 500, ethers.constants.AddressZero, 0],   // Josip
+        [3, "QmRde9qaDmyDrxEpHPC5ycnt36S375WUaBpSrDuemE68Vn", "", 50, developer.address, 30000]            // Glenn
+    ];
+
+    // add assets
+    await contentManager.connect(developer).addAssetBatch(asset);
+    
+    // mint some assets
+    var mintData = [developer.address, [0,1,2,3], [100,100,100,10], 0, ethers.constants.AddressZero, []];
+    await content.connect(developer).mintBatch(mintData);
+}
+
+async function addSuperScaryHorrorGameAssets(content, contentManager, developer) {
+    var asset = [
+        [0, "QmXnMAmDwKuMsmWLc6dnccwaGqLMXyGHBjEvC9oHePNvFX", "", 10000, ethers.constants.AddressZero, 0], // Scary Terry
+        [1, "QmfMoQt1LGs7PN5UYP2kikkuYpHJiFHGx2EHCPaEuLZ7Kt", "", 50, ethers.constants.AddressZero, 0],    // Casper the Ghost
+        [2, "QmRpA8dRsLhzwGifk2yV4fcTJpAP89s3Mmm5X1qqH5Xwza", "", 50, ethers.constants.AddressZero, 0],    // Screamer
+        [3, "QmZMpYizqKeSVFtQrWWYSmPK93yWHJgFsvFYZuKaQz8QXn", "", 25, ethers.constants.AddressZero, 0]     // Headless Canadian
+    ];
+
+    // add assets
+    await contentManager.connect(developer).addAssetBatch(asset);
+    
+    // mint some assets
+    var mintData = [developer.address, [0,1,2,3], [1000,50,50,25], 0, ethers.constants.AddressZero, []];
+    await content.connect(developer).mintBatch(mintData);
+}
+
+async function main() {
+    const [
+        deployer,
+        dev1,
+        dev2,
+        dev3,
+        player1,
+        player2,
+        player3,
+        player4
+    ] = await ethers.getSigners();
+
+    console.log(`Deploying contracts with the account: ${deployer.address}`);
+  
+    var balance = await deployer.getBalance();
+    console.log(`Account Balance: ${balance.toString()}\n`);
+
+    // Get Content Contract Factory 
+    Content = await ethers.getContractFactory("Content");
+    ContentManager = await ethers.getContractFactory("ContentManager");
+    const ContentFactory = await ethers.getContractFactory("ContentFactory");
+    const factory = ContentFactory.attach("0x2385547DAd794d7cddc46ecF9ab8CCa803981FAf");
+
+    // Note: for the URI, the developer has to add "https://ipfs.io/ipfs/" before downloading from ipfs
+    // Developer 1 Rawrshak and Scream Fortress 2 Contract
+    var addresses = await deployContract(factory, dev1, 10000, "QmQigPDu4wChRtrdgnbCFJFuYHD55sioFztdu1r9bME46o");
+    console.log(`Rawrshak Contracts: Content[ ${addresses.content.address} ], ContentManager[ ${addresses.contentManager.address} ]\n`);
+    await addRawrshakAssets(addresses.content, addresses.contentManager, dev1);
+
+    var addresses = await deployContract(factory, dev1, 10000, "QmP3Hnw72H4w7s4XEEa12vHAa7EYXSCGEd2KPcScrMvx9z");
+    console.log(`ScreamFortress2 Contracts: Content[ ${addresses.content.address} ], ContentManager[ ${addresses.contentManager.address} ]\n`);
+    await addScreamFortress2Assets(addresses.content, addresses.contentManager, dev1);
+    
+    // Developer 2 Deploys A Contract
+    addresses = await deployContract(factory, dev2, 20000, "QmSkixBkwkEoMzEVcWsDxaYxVhEJDiatPjAEvHaJY383Ks");
+    console.log(`FightBuddy Contracts: Content[ ${addresses.content.address} ], ContentManager[ ${addresses.contentManager.address} ]\n`);
+    await addFightBuddyAssets(addresses.content, addresses.contentManager, dev2);
+    
+    // Developer 3 Deploys A Contract
+    addresses = await deployContract(factory, dev3, 15000, "QmY1cxbALonbMiSTWnnQooCJqKDcAMs69nqhmBP8jSyQP8");
+    console.log(`SuperScaryHorrorGame Contracts: Content[ ${addresses.content.address} ], ContentManager[ ${addresses.contentManager.address} ]\n`);
+    await addSuperScaryHorrorGameAssets(addresses.content, addresses.contentManager, dev3);
+
+    balance = await deployer.getBalance();
+    balance = web3.utils.fromWei(balance.toString(), 'ether');
+    console.log(`Account Balance: ${balance.toString()}`);
+  }
+  
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
+
+// // Optimism Kovan Contracts
+// Rawrshak Contracts: Content[ 0xd0938b7fDB19de29c85f90BCBe33c094a29AE285 ], ContentManager[ 0x3118d2EA613ae7659b3861680D3B470Db16Cc0d8 ]
+// ScreamFortress2 Contracts: Content[ 0x184d723b301C08401F200a4CDF221c5FC93Df3E5 ], ContentManager[ 0x147047C2d6F3a4dBa809B2B4E6703A58f6a8B933 ]
+// FightBuddy Contracts: Content[ 0x95aB4096a5a782Caa96D5dEC502fDD60b820E6AD ], ContentManager[ 0xF0C40E2853Cdf0eDdeB86cc82d6C4c3300bCAcdd ]
+// SuperScaryHorrorGame Contracts: Content[ 0xeCa6268Da026dAc239E790044eE2eBe75e075F40 ], ContentManager[ 0x7155f181138C8bd2FC010eD7aEA976f278cC4D5b ]
