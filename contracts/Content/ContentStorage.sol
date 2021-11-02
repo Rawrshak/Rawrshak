c220bb9d142b20749c0c50eaa6bdcdac811a1913
butterfiolee <butterfiolee@gmail.com>
2021-11-02 17:47:52 -0400

    Added content contract tests

--- contracts/Content/ContentStorage.sol
+++ contracts/Content/ContentStorage.sol
@@ -166,8 +166,7 @@ contract ContentStorage is IContentStorage, AccessControlUpgradeable, HasRoyalty
     * @dev returns the royalty receiver address and rate for a token
     */
     function getRoyalty(uint256 _tokenId) external view override onlyRole(DEFAULT_ADMIN_ROLE) returns (address receiver, uint24 rate) {
-        // If token id doesn't exist or there isn't a royalty fee attached to this specific token, 
-        // _getRoyalty() will return the contract's default royalty fee. However, that can also
+        // If token id doesn't exist, _getRoyalty() will return the contract's default royalty fee. However, that can also
         // be null. In the case of null, there are no royalty fees. 
         return _getRoyalty(_tokenId);
     }
