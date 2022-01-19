505713b9ed9689bd2d0e7848f1cf163471626c05
butterfiolee <90117690+butterfiolee@users.noreply.github.com>
2022-01-19 17:42:15 -0500

    V2-Feature: Unique Asset Content Feature (#48)
    * add unique content feature
    
    * fixed errors for change request
    
    * fixed more change requests
    
    * fixed events

---
+++ contracts/Content/interfaces/IMultipleRoyalties.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IMultipleRoyalties {
+    /******** View Functions ********/
+    function multipleRoyaltyInfo(uint256 _uniqueId, uint256 _saleprice) external view returns(address[] memory, uint256[] memory);
+}
