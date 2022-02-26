6aeffb1e5be0dfda0c5bac570eeffd34df4e1204
Christian Sumido <gcbsumid@gmail.com>
2022-02-25 19:10:32 -0800

    Added back resolver tests, added hardhat-contract-sizer, updated readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,18 @@ require('@openzeppelin/hardhat-upgrades');
 require("@nomiclabs/hardhat-web3");
 require('@typechain/hardhat');
 require('@nomiclabs/hardhat-ethers');
+require('hardhat-contract-sizer');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.9",
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true,
+    only: [],
+  },
 };
---
+++ test/resolver/AddressResolverTests.js
@@ -0,0 +1,68 @@
+const { expect, assert } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+describe('Address Resolver Contract tests', () => {
+    var deployerAddress;
+    var resolver;
+
+    before(async () => {
+        [deployerAddress] = await ethers.getSigners();
+        AddressResolver = await ethers.getContractFactory("AddressResolver");
+        MockToken = await ethers.getContractFactory("MockToken");
+        ContentFactory = await ethers.getContractFactory("ContentFactory");
+    });
+
+    beforeEach(async () => {
+        resolver = await upgrades.deployProxy(AddressResolver, []);
+        rawr = await upgrades.deployProxy(MockToken, ["Rawrshak Token", "RAWR"]);
+
+        // Initialize Content Clone Factory
+        contentFactory = await upgrades.deployProxy(ContentFactory, [deployerAddress.address, deployerAddress.address, deployerAddress.address, deployerAddress.address]);
+    });
+
+    describe("Basic Tests", () => {
+        it('Check if address resolver was deployed properly', async () => {
+            expect(resolver.address).not.equal(ethers.constants.Address);
+        });
+
+        it('Supports the Address resolver Interface', async () => {
+            expect(await resolver.supportsInterface("0x9c7ff313")).to.equal(true);
+        });
+    });
+
+    describe("Register Contracts", () => {
+        it('Register a single contract', async () => {    
+            // CONTRACT_NFT_ESCROW
+            var ids = ["0x3d13c043"];
+            var addresses = [rawr.address];
+    
+            await expect(await resolver.registerAddress(ids, addresses))
+                .to.emit(resolver, 'AddressRegistered');
+
+            expect(await resolver.getAddress("0x3d13c043")).to.equal(rawr.address);
+        });
+    
+        it('Register multiple contracts', async () => {
+            var ids = ["0x3d13c043", "0xdb337f7d"];
+            var addresses = [rawr.address, contentFactory.address];
+    
+            await expect(await resolver.registerAddress(ids, addresses))
+                .to.emit(resolver, 'AddressRegistered');
+    
+            expect(await resolver.getAddress("0x3d13c043")).to.equal(rawr.address);
+            expect(await resolver.getAddress("0xdb337f7d")).to.equal(contentFactory.address);
+        });
+     
+        it('Register test input length mismatch', async () => {
+            var ids = ["0x3d13c043", "0xdb337f7d"];
+            var addresses = [rawr.address];
+            
+            await expect(resolver.registerAddress(ids, addresses)).to.be.reverted;
+            await expect(resolver.registerAddress([], addresses)).to.be.reverted;
+        });
+     
+        it('Invalid id', async () => {
+            await expect(resolver.getAddressWithCheck("0x00000001")).to.be.reverted;
+        });
+    });
+});
