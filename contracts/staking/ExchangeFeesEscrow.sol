686141b41f7fca477980290e2f69ffd3a2878f6d
Christian Sumido <gcbsumid@gmail.com>
2022-02-25 16:50:51 -0800

    refactored escrowbase

--- contracts/staking/ExchangeFeesEscrow.sol
+++ contracts/staking/ExchangeFeesEscrow.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "./EscrowBase.sol";
+import "../escrow/EscrowBase.sol";
 import "../resolver/IAddressResolver.sol";
 import "./interfaces/IExchangeFeesEscrow.sol";
 import "./interfaces/IStaking.sol";
